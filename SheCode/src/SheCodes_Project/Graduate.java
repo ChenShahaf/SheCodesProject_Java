d2f31e35b831a057ac5d9f549392114dc0351a0e
chen <cshahaf@gmail.com>
2021-11-03 09:39:42 +0200

    FINAL VERSION

--- SheCode/src/SheCodes_Project/Graduate.java
+++ SheCode/src/SheCodes_Project/Graduate.java
@@ -1,74 +1,211 @@
 package SheCodes_Project;
 
-import java.net.URL;
 import java.util.*;
 
 
+
+
+
 public class Graduate {
-	//properties
-    String firstName;
-    String lastName;
-    String password;
-    boolean isNeedToChangePass;
-	List<String> courses;
-	URL Linkedin;
+	// properties
+	String userName;
+	static String password;
+	boolean isNeedToChangePass = true;
+	ArrayList<String> courses = new ArrayList<String>();
+
 	JobSearchingStatus jobSearchingStatus;
-	
-public Graduate(){
-	//String user = createUserName(firstName, lastName);
-	char[] otpPass;
-	
-	
-	otpPass = otpBuild();
-	System.out.println("Your OTP is: " + otpPass);
-	
-	
-	
-}
-	
+	String urlLink;
+
+	public Graduate(String i_firstName, String i_lastName) {
 
 
-static public String createUserName(String firstName, String lastName){
-	 String userName = firstName + "." + lastName + ".she-codes";
-	 System.out.println("Your user name is:" + userName);
-	 return userName;
-}
-
-static char[] otpBuild (){
-	
-
-    // Using numeric values
-    String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    String Small_chars = "abcdefghijklmnopqrstuvwxyz";
-    String numbers = "0123456789";
-            
+		//creating user
+		userName = createUserName(i_firstName, i_lastName);
+		//creating OTP Password
+		password = otpBuilder();
+		System.out.println("Your OTP is: " + password);
 
 
-    String values = Capital_chars + Small_chars +
-                    numbers;
-
-    // Using random method
-    Random rndm_method = new Random();
-
-    char[] otp = new char[8];
-
-    char[] password = new char[8];
-    
-    for (int i = 0; i < 8; i++)
-    {
-        // Use of charAt() method : to get character value
-        // Use of nextInt() as it is scanning the value as int
-        password[i] =
-          values.charAt(rndm_method.nextInt(values.length()));
-
-    }
-    return password;
-}
-
-
-	enum JobSearchingStatus {
-		NOTLOOKING,
-		OPEN,
-		SEARCHING	
 	}
-}
+
+	private String createUserName(String firstName, String lastName) {
+		String userName = firstName + "." + lastName + ".she-codes";
+		System.out.println("Your user name is:" + userName);
+		return userName;
+	}
+
+	private String otpBuilder() {
+
+		// Using numeric values
+		String passOTP = null;
+
+		String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String Small_chars = "abcdefghijklmnopqrstuvwxyz";
+		String numbers = "0123456789";
+
+		String values = Capital_chars + Small_chars + numbers;
+		// Using random method
+		Random rndm_method = new Random();
+
+
+		char[] passW = new char[8];
+
+		for (int i = 0; i < 8; i++) {
+			// Use of charAt() method : to get character value
+			// Use of nextInt() as it is scanning the value as int
+			passW[i] = values.charAt(rndm_method.nextInt(values.length()));
+			passOTP = String.valueOf(passW);
+
+
+		}
+
+		return passOTP;
+	}
+
+	private static boolean isValidPassword(String  i_password ) {
+
+		if ( i_password.length() < 8 ||  i_password.length() > 8)
+		{
+			System.out.println("Your password must be 8 characters long");
+			return false;
+		}
+		String upperCaseChars = "(.*[A-Z].*)";
+		if (! i_password.matches(upperCaseChars ))
+		{
+			System.out.println("Your password must contain atleast one capital letter, olower case letter and one number");
+			return false;
+		}
+		String lowerCaseChars = "(.*[a-z].*)";
+		if (! i_password.matches(lowerCaseChars ))
+		{
+			System.out.println("Your password must contain atleast one capital letter, olower case letter and one number");
+			return false;
+		}
+		String numbers = "(.*[0-9].*)";
+		if (! i_password.matches(numbers ))
+		{
+			System.out.println("Your password must contain atleast one capital letter, olower case letter and one number");
+			return false;
+		}
+
+		else {
+			return true;
+		}
+
+	}
+
+
+	public void updatePassword() {
+		String  updatedPass = "";
+		Scanner scan = new Scanner(System.in);
+		System.out.println("please type in your updated password it should contain 8 charcters small letters, caps and numbers");
+		updatedPass = scan.next();
+		while(!isValidPassword(updatedPass)) {
+			System.out.println("Your password must contain atleast one capital letter, olower case letter and one number");
+			System.out.println("please select a valid password");
+			updatedPass = scan.next();
+		}
+		if(isValidPassword(updatedPass)) {
+			password = updatedPass;
+		}
+	}
+	enum JobSearchingStatus {
+		NOTLOOKING, OPEN, SEARCHING
+	}
+
+	public void updateResume() {
+		System.out.println("which of the following courses would you like to add to your resume by alphabetical order?");
+		System.out.println("\"Android\",\"Basic Python\",\"Data analysis\",\"Git\",\"Java basics\",\n"
+				+ "\"Landing a job \",\"Git\",\"Python for programmers\",\"React\"");
+		Scanner scan =new Scanner(System.in);
+		boolean addCourses = true;
+		while(addCourses) {
+			System.out.println("enter a course you wish to add:");
+			String course =scan.nextLine();
+			System.out.println(course);
+			if(!courses.contains(course)){
+				courses.add(course);
+			}
+			System.out.println("would you like to add another course: 'Y' or 'N'");
+			char i_addCourses = scan.next().charAt(0);
+
+
+			if(i_addCourses == 'Y' || i_addCourses == 'y') {
+				addCourses = true;
+			}
+
+
+
+
+			else if(i_addCourses == 'N' || i_addCourses == 'n') {
+				addCourses = false;
+
+			}
+			else {
+				System.out.println("invalid input");
+				break;
+			}
+		}
+	}
+
+	public void updateLinkedin() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please add the link of the LinkedIn page:");
+		urlLink = scan.next();
+
+	}
+
+	public void updateJobSreachStatus(){
+		Scanner scan = new Scanner(System.in);
+		int jobSearch;
+		System.out.println("Please choose one of the three options:");
+		System.out.println("1.NOTLOOKING 2.OPEN 3.SEARCHING");
+		jobSearch = scan.nextInt();
+		switch(jobSearch){
+		case 1:
+			System.out.println("you have chosen Not looking");
+			jobSearchingStatus = JobSearchingStatus.NOTLOOKING;
+			break;
+
+		case 2:
+			System.out.println("you have chosen open");
+			jobSearchingStatus = JobSearchingStatus.OPEN;
+			break;
+
+		case 3:
+			System.out.println("you have chosen Searching");
+			jobSearchingStatus = JobSearchingStatus.SEARCHING;
+			break;
+
+		default:
+			System.out.println("You have chosen an unvalid answer the default is not looking");
+			jobSearchingStatus = JobSearchingStatus.NOTLOOKING;
+			break;
+
+		}
+
+	}
+
+
+	public boolean shouldOfferJob(ArrayList<String> requirements){
+		//System.out.println(courses.toString());
+		if (jobSearchingStatus == JobSearchingStatus.NOTLOOKING)
+			return false;
+		if(courses.size() < requirements.size()) {
+			return false;
+		}
+
+		for(int j=0; j < requirements.size(); j++) {
+			if (!courses.contains(requirements.get(j))) 
+				return false;
+		}
+		return true;
+	}
+
+
+
+}//end of class
+
+
+
+
--- SheCode/src/SheCodes_Project/Main.java
+++ SheCode/src/SheCodes_Project/Main.java
@@ -2,106 +2,127 @@ package SheCodes_Project;
 
 import java.util.*;
 
-public class Main {
+/**
+ * @author chen
+ *
+ */
+public class Main {//begining of class
 
-    
+     static ArrayList<Graduate> graduates = new ArrayList<Graduate>();
 
 
 	public static void main(String[] args) {
-		Graduate alumni1 = new Graduate();
+		
+		ArrayList<String> requirements = new ArrayList<String>();
+		requirements.add("Basic Python");
+		requirements.add("Git");
+		boolean i_shouldOfferJob = false;
+		
+		Graduate alumni1 = registerNewGrad("Chen","Shahaf");
+		graduates.add(alumni1);
+		Graduate alumni2 = registerNewGrad("Inbal","Schkolnik");
+		graduates.add(alumni2);
 		
 		
-		/*alumniID alumni1 = new alumniID();
-		String newPass;
-		alumniID.isValidID(alumni1.firstName);
-		alumniID.isValidID(alumni1.lastName);
-		OTP otp = new OTP(8);
-		char[] firstPass = OTP.otpBuild(otp.len);
-		System.out.println(firstPass);
+		
+		
+		Graduate chen = sheCodesLogin(alumni1.userName, alumni1.password);
+		if(chen != null) {
+			if(chen.isNeedToChangePass) {
+				chen.updatePassword();
+			}
+		}
+		
+	
+		
+		
+		Graduate inbal = sheCodesLogin(alumni2.userName, alumni2.password);
+		if(inbal != null) {
+			if(inbal.isNeedToChangePass) {
+				inbal.updatePassword();
+			}
+		}
+		
+		
+		updatingLoop(chen);
+		updatingLoop(inbal);
 		
 		
 		
-		//creating new password
-		newPass = OTP.changePassword();
-		System.out.println(OTP.isValidPassword(newPass));
 		
-		String [] resume = new String[1];
-		resume = updateResume(resume);
-		
-		
-		LinkedIn linkIn = new LinkedIn();
-		LinkedIn.validLink(linkIn.linkedin);
-		int jobSearch = jobSearchStatus();
-		
-		//calling registergrad to create user name
-		String userName = registerNewGradUserName(alumni1.firstName,alumni1.lastName);
-		
-		
-		//creating update variable for update
-		int update = registerNewGradUpdae();
-		*/
-		 
-		
-		
+		i_shouldOfferJob = chen.shouldOfferJob(requirements);
+		System.out.println(i_shouldOfferJob);
+		i_shouldOfferJob = inbal.shouldOfferJob(requirements);
+		//System.out.println(i_shouldOfferJob);
+	
 
-		
-	        
+	}//end of main 
 
+	public static Graduate registerNewGrad(String firstname, String lastname) {
+		return new Graduate(firstname,lastname);
+		
 	}
-static public int jobSearchStatus() {
-		
+	
+	
+	public static Graduate sheCodesLogin(String username, String password) {
+		for(int i=0; i < graduates.size(); i++) {
+			if(graduates.get(i).userName == username){
+				if(graduates.get(i).password == password) {
+					return graduates.get(i);
+				}
+			}
+		}
+		return null;
+	}
+	public static void updatingLoop(Graduate graduate) {
+		int option;
+		char i_continue;
+		boolean continueloop = true;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("welcome student which job search status are you now?");
-		System.out.println("1. not looking for a change");
-		System.out.println("2. not looking but open for suggestions");
-		System.out.println("3. looking for a new challenge");
-		int decision = scan.nextInt();
-		return decision;
-	}
-	static public String[] updateResume(String[] courses) {
 		
-		 System.out.println("which of the following courses would you like to add to your resume?");
-		 System.out.println("\"Web\",\"Java basics\",\"Basic Python\",\"Android\",\"Python for programmers\",\n"
-		 		+ "\"Data analysis \",\"Git\",\"Landing a job\",\"React\"");
-		 Scanner scan =new Scanner(System.in);
-		 System.out.println("enter the number of courses you learned and wish to added");
-		 int n=scan.nextInt();
-		 courses =new String[n];
-		 for(int i=0;i<n;i++){
-		        System.out.println("enter the "+(i+1)+" course you wish to add with no spaces:");
-		        courses[i]=scan.next();
-		    }
-		    System.out.println("courses Entered by user:");
-
-		    for(String s:courses){
-		        System.out.println(s);
-		    }
-		    
-		    return courses;
-		
+		while(continueloop) {
+			System.out.println("Hello " + graduate.userName);
+			System.out.println("What would you like to do:");
+			System.out.println("1. update Password");
+			System.out.println("2. update resume");
+			System.out.println("3. update linkdenIn page");
+			System.out.println("4. update job search status");
+			option = scan.nextInt();
+			switch(option) {
+				case 1:
+					graduate.updatePassword();
+					break;
+				case 2:
+					graduate.updateResume();
+					graduate.updateJobSreachStatus();
+					break;
+				case 3:
+					graduate.updateLinkedin();
+					break;
+				case 4:
+					graduate.updateJobSreachStatus();
+					break;
+				default:
+					System.out.println("Nothing from 1-4 chosen");
+					continueloop = false;
+					break;
+			}
+				
+			System.out.println("Would you like to update something else type Y/N?");
+			 i_continue = scan.next().charAt(0);
+			if(i_continue == 'Y' || i_continue == 'y') {
+				continueloop = true;
+			}
+			else if(i_continue == 'N' || i_continue == 'n') {
+				continueloop = false;
+				
+			}
+			else {
+				System.out.println("invalid input");
+				continueloop = false;
+			}
+		}
 	}
+	
 
-	public static String registerNewGradUserName(String firstName, String lastName){
-		 String user = firstName + "." + lastName + ".she-codes";
-		 System.out.println("Your user name is:" + user);
-		 return user;
-		 
-	 }
-	public static int registerNewGradUpdae() {
-		String newPassword = null;
-		System.out.println("which of the following would you like to do:");
-		System.out.println("1. update password");
-		System.out.println("2. update resume");
-		System.out.println("3. update job search status");
-		System.out.println("4. update your linkedin page?");
-		System.out.println("5. i wish to update nothing");
-		Scanner scan = new Scanner(System.in);
-		int update = scan.nextInt();
-		 return update;
-	}
-	
-	
-	
-	 
-
-}
+}//end of class
